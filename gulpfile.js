6bd05ef035924d1f613a5ee1b9dfb9420faf6744
Eruant <info@littleball.co.uk>
2014-02-01 12:12:49 +0000

    Added patching options

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,12 @@
  **/
 
 var gulp = require('gulp'),
+		util = require('gulp-util'),
 		jshint = require('gulp-jshint'),
 		jasmine = require('gulp-jasmine'),
-		minifyHTML = require('gulp-minify-html');
+		concat = require('gulp-concat'),
+		minifyHTML = require('gulp-minify-html'),
+		bump = require('gulp-bump');
 
 /**
  * scripts
@@ -33,13 +36,37 @@ gulp.task('scripts-test', function () {
 gulp.task('markup', ['markup-compress']);
 
 gulp.task('markup-compress', function () {
-	return gulp.src('src/html/*.html')
-		.pipe(minifyHTML({
-			comments: false
-		}))
-		.pipe(gulp.dest('bin/'));
+
+	if (util.env.production) {
+		return gulp.src('src/html/*.html')
+			.pipe(minifyHTML({
+				comments: false
+			}))
+			.pipe(gulp.dest('bin/'));
+	} else {
+		return gulp.src('src/html/*.html')
+			.pipe(gulp.dest('bin/'));
+	}
+});
+
+gulp.task('bump', function () {
+
+	opts = {
+		type: "patch"
+	};
+	if (util.env.type === 'major') {
+		opts.type = "major";
+	} else if (util.env.type === 'minor') {
+		opts.type = "minor";
+	}
+
+	return gulp.src('package.json')
+		.pipe(bump(opts))
+		.pipe(gulp.dest('./'));
 });
 
 gulp.task('compile', ['scripts', 'markup']);
+
+gulp.task('patch', ['compile', 'bump']);
 
 gulp.task('default', ['compile']);
