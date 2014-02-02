8d2633fae8727bbb791afd677cdbc74e66c7c3d9
Eruant <info@littleball.co.uk>
2014-02-02 23:17:19 +0000

    Added song view

--- src/js/root.js
+++ src/js/root.js
@@ -2,23 +2,34 @@ var app = angular.module('notesApp', ['ngRoute']);
 
 app.config(function ($routeProvider) {
 	
-	$routeProvider
-		.when('/', {
-			controller: 'LibraryController',
-			templateUrl: 'app/views/library.html'
-		});
+	$routeProvider.when('/', {
+		controller: 'LibraryController',
+		templateUrl: 'app/views/library.html'
+	}).when('/song', {
+		controller: 'SongController',
+		templateUrl: 'app/views/song.html'
+	});
+
 });
 
 app.controller('LibraryController', function ($scope, libraryFactory) {
 
-	function init() {
-		libraryFactory.getSongs().success(function (data) {
+	libraryFactory.getSongs()
+		.success(function (data) {
 			$scope.songs = data;
+		})
+		.error(function (error) {
+			window.console.warn(error);
 		});
-	}
 
-	init();
+});
 
+app.controller('SongController', function ($scope) {
+	$scope.song = {
+		"title": "A Test song",
+		"author": "Matt Gale",
+		"body": "This is my song"
+	};
 });
 
 app.factory('libraryFactory', function ($http) {
