7c987597eef89e52aef79f9334ffa0469b7b86ee
Matt Gale <matt@littleball.co.uk>
2014-02-03 11:41:03 +0000

    Converted angular to use browserify

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,17 @@
  * Tasks to make development easier
  *
  **/
+/*globals require*/
 
 var gulp = require('gulp'),
-		util = require('gulp-util'),
-		jshint = require('gulp-jshint'),
-		jasmine = require('gulp-jasmine'),
-		browserify = require('gulp-browserify'),
-		concat = require('gulp-concat'),
-		uglify = require('gulp-uglify'),
-		minifyHTML = require('gulp-minify-html'),
-		bump = require('gulp-bump');
+    util = require('gulp-util'),
+    jshint = require('gulp-jshint'),
+    jasmine = require('gulp-jasmine'),
+    browserify = require('gulp-browserify'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    minifyHTML = require('gulp-minify-html'),
+    bump = require('gulp-bump');
 
 /**
  * scripts
@@ -21,42 +22,34 @@ var gulp = require('gulp'),
 gulp.task('scripts', ['scripts-validate', 'scripts-test', 'scripts-bundle']);
 
 gulp.task('scripts-validate', function () {
-	return gulp.src(['src/js/modules/*.js', 'src/js/*.js'])
-		.pipe(jshint('.jshintrc'))
-		.pipe(jshint.reporter('default'));
+  return gulp.src(['src/js/modules/*.js', 'src/js/*.js'])
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'));
 });
 
 gulp.task('scripts-test', function () {
-	return gulp.src('test/*.js')
-		.pipe(jasmine());
+  return gulp.src('test/*.js')
+    .pipe(jasmine());
 });
 
-/*
 gulp.task('scripts-bundle', function () {
 
-	if (util.env.production) {
-		return gulp.src('src/js/root.js')
-			.pipe(browserify({
-				debug: true
-			}))
-			.pipe(concat('bundle.js'))
-			.pipe(uglify())
-			.pipe(gulp.dest('bin/js'));
-	} else {
-		return gulp.src('src/js/root.js')
-			.pipe(browserify({
-				debug: true
-			}))
-			.pipe(concat('bundle.js'))
-			.pipe(gulp.dest('bin/js'));
-	}
-});
-*/
-
-gulp.task('scripts-bundle', function () {
-	return gulp.src('src/js/**/*.js')
-		.pipe(concat('bundle.js'))
-		.pipe(gulp.dest('bin/js'));
+  if (util.env.production) {
+    return gulp.src('src/js/root.js')
+      .pipe(browserify({
+        debug: true
+      }))
+      .pipe(concat('bundle.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest('bin/js'));
+  } else {
+    return gulp.src('src/js/root.js')
+      .pipe(browserify({
+        debug: true
+      }))
+      .pipe(concat('bundle.js'))
+      .pipe(gulp.dest('bin/js'));
+  }
 });
 
 /**
@@ -67,37 +60,37 @@ gulp.task('markup', ['markup-compress']);
 
 gulp.task('markup-compress', function () {
 
-	if (util.env.production) {
-		return gulp.src('src/html/*.html')
-			.pipe(minifyHTML({
-				comments: false
-			}))
-			.pipe(gulp.dest('bin/'));
-	} else {
-		return gulp.src('src/html/**/*.html')
-			.pipe(gulp.dest('bin/'));
-	}
+  if (util.env.production) {
+    return gulp.src('src/html/*.html')
+      .pipe(minifyHTML({
+        comments: false
+      }))
+      .pipe(gulp.dest('bin/'));
+  } else {
+    return gulp.src('src/html/**/*.html')
+      .pipe(gulp.dest('bin/'));
+  }
 });
 
 gulp.task('data', function () {
-	return gulp.src('src/data/*.json')
-		.pipe(gulp.dest('bin/data'))
+  return gulp.src('src/data/*.json')
+    .pipe(gulp.dest('bin/data'))
 });
 
 gulp.task('bump', function () {
 
-	opts = {
-		type: "patch"
-	};
-	if (util.env.type === 'major') {
-		opts.type = "major";
-	} else if (util.env.type === 'minor') {
-		opts.type = "minor";
-	}
+  opts = {
+    type: "patch"
+  };
+  if (util.env.type === 'major') {
+    opts.type = "major";
+  } else if (util.env.type === 'minor') {
+    opts.type = "minor";
+  }
 
-	return gulp.src('package.json')
-		.pipe(bump(opts))
-		.pipe(gulp.dest('./'));
+  return gulp.src('package.json')
+    .pipe(bump(opts))
+    .pipe(gulp.dest('./'));
 });
 
 gulp.task('compile', ['scripts', 'markup', 'data']);
--- src/js/root.js
+++ src/js/root.js
@@ -1,41 +1,46 @@
+/*globals require, angular*/
+
+require('angular/angular');
+require('angular-route/angular-route');
+
 var app = angular.module('notesApp', ['ngRoute']);
 
 app.config(function ($routeProvider) {
-	
-	$routeProvider.when('/', {
-		controller: 'LibraryController',
-		templateUrl: 'app/views/library.html'
-	}).when('/song', {
-		controller: 'SongController',
-		templateUrl: 'app/views/song.html'
-	});
+  
+  $routeProvider.when('/', {
+    controller: 'LibraryController',
+    templateUrl: 'app/views/library.html'
+  }).when('/song', {
+    controller: 'SongController',
+    templateUrl: 'app/views/song.html'
+  });
 
 });
 
 app.controller('LibraryController', function ($scope, libraryFactory) {
 
-	libraryFactory.getSongs()
-		.success(function (data) {
-			$scope.songs = data;
-		})
-		.error(function (error) {
-			window.console.warn(error);
-		});
+  libraryFactory.getSongs()
+    .success(function (data) {
+      $scope.songs = data;
+    })
+    .error(function (error) {
+      window.console.warn(error);
+    });
 
 });
 
 app.controller('SongController', function ($scope) {
-	$scope.song = {
-		"title": "A Test song",
-		"author": "Matt Gale",
-		"body": "This is my song"
-	};
+  $scope.song = {
+    "title": "A Test song",
+    "author": "Matt Gale",
+    "body": "This is my song"
+  };
 });
 
 app.factory('libraryFactory', function ($http) {
-	var factory = {};
-	factory.getSongs = function () {
-		return $http.get('/data/songs.json');
-	};
-	return factory;
+  var factory = {};
+  factory.getSongs = function () {
+    return $http.get('/data/songs.json');
+  };
+  return factory;
 });
