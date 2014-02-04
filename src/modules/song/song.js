4cf478745d4c75793fe6193fdc78475ffcc28dd3
Eruant <matt@littleball.co.uk>
2014-02-04 22:48:53 +0000

    made song list dynamic

--- src/modules/song/song.js
+++ src/modules/song/song.js
@@ -4,18 +4,20 @@ exports.init = function (app) {
 
 	app.config(function ($routeProvider) {
 		
-		$routeProvider.when('/song', {
+		$routeProvider.when('/song/:songId', {
 			controller: 'songController',
 			templateUrl: './modules/song/song.html'
+		}).otherwise({
+			redirectTo: '/library'
 		});
 
 	});
 
-  app.controller('songController', function ($scope, songFactory) {
+  app.controller('songController', function ($scope, $routeParams, songFactory) {
 
     songFactory.getSongs()
       .success(function (data) {
-        $scope.song = data[0];
+        $scope.song = data[$routeParams.songId];
       })
       .error(function (error) {
         window.console.warn(error);
